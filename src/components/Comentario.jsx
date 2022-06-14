53aa044f36650e82466174ec3d2bf6e5bf692451
Sean <sean.saez.f@gmail.com>
2022-06-14 15:16:23 +0200

    Usando componentes con otros componentes

---
+++ src/components/Comentario.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import Avatar from './Avatar';
+
+
+const Comentario = ({sujeto}) => {
+    return ( 
+        <Fragment>
+            <h1>Comentarios</h1>
+            <hr />
+            <hr />
+            <div className="media">
+                
+                <Avatar urlImagen={sujeto.urlImagen} />
+                <div className="media-body">
+                    <h5 className="mt-0">{sujeto.nombre}</h5>
+                    {sujeto.texto}
+                </div>
+            </div>
+        </Fragment>
+     );
+}
+ 
+export default Comentario;
